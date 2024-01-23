71a73e6463bc79558800df2bd5f0b993e54b49b4
Evelyn Cristina <147438447+EvelynAires@users.noreply.github.com>
2024-01-22 22:40:52 -0300

    Update 04-Union.c

--- unidade 1/04-Union.c
+++ unidade 1/04-Union.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
 
+typedef union tipodados{
+    int meu_int;
+    float meu_float;
+}Dados;
+
+int main(void){
+    Dados uniao;
+    printf("Digite os dados: \t");
+    scanf("%d %f", &uniao.meu_int, &uniao.meu_float);
+    printf("OS DADOS INFORMADOS: meu_int = %d meu_float=%f", uniao.meu_int, uniao.meu_float);
+    return 0;
+}
