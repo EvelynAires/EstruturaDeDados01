a4a14cb12675347becc761017bc37ac23fbfc229
Evelyn Cristina <147438447+EvelynAires@users.noreply.github.com>
2024-01-24 19:18:33 +0000

    Atualização

---
+++ unidade 1/05-Enum.c/Aulas/01-Anotação.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define TRUE 0
+#define FALSE 1
+
+int main(void){
+    int resposta;
+    printf("VocÊ gosta de algoritmos?\n 0-True\n 1-False\n");
+    scanf("%d", &resposta);
+    if(resposta==TRUE){
+        printf("Boa escolha\n");
+    }
+    else if(resposta==FALSE){
+        printf("Que pena!\n");
+    }
+    return 0;
+}
