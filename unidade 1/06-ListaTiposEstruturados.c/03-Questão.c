08a9d2272d9fa921097ec30a023a4e5871730feb
Evelyn Cristina <147438447+EvelynAires@users.noreply.github.com>
2024-01-29 02:25:35 +0000

    .

--- unidade 1/06-ListaTiposEstruturados.c/03-Questão.c
+++ unidade 1/06-ListaTiposEstruturados.c/03-Questão.c
@@ -5,3 +5,70 @@
 //Protótipo: Ret* ret_circunscrito(Circ* c, float h);
 //b) Dado um retângulo, crie e retorne o maior círculo interno ao retângulo.
 //Protótipo: Circ* circ_interno(Ret* r);
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct ret {
+    float base;
+    float altura;
+} Ret;
+
+typedef struct circ {
+    float raio;
+} Circ;
+
+Ret* ret_circunscrito(Circ* circ, float h) {
+    Ret* ret = (Ret*)malloc(sizeof(Ret));
+
+    float lado = sqrt(pow(2 * circ->raio, 2) - pow(h, 2));
+
+    ret->base = lado;
+    ret->altura = h;
+    return ret;
+}
+
+Circ* circ_interno(Ret* r) {
+    Circ* circ = (Circ*)malloc(sizeof(Circ));
+
+    circ->raio = (r->base < r->altura) ? r->base / 2 : r->altura / 2;
+
+    return circ;
+}
+
+
+int main() {
+    Ret* Retangulo;
+    Circ* Circulo;
+
+    Circulo = (Circ*)malloc(sizeof(Circ));
+    Retangulo = (Ret*)malloc(sizeof(Ret));
+
+    if (Circulo == NULL || Retangulo == NULL) {
+        printf("Erro de alocação de memória.\n");
+        return 1;
+    }
+
+    float h;
+
+    printf("Informe o raio do círculo: ");
+    scanf("%f", &Circulo->raio);
+
+    printf("Informe qual a altura do retangulo circunscrito: ");
+    scanf("%f", &h);
+
+    Retangulo = ret_circunscrito(Circulo, h);
+
+    printf("O maior retangulo circunscrito tem a base: %.2f e a altura: %.2f\n", Retangulo->base, Retangulo->altura);
+
+    Circulo = circ_interno(Retangulo);
+
+    printf("O maior círculo interno tem raio de: %.2f.\n", Circulo->raio);
+
+    free(Circulo);
+    free(Retangulo);
+
+    return 0;
+}
+
