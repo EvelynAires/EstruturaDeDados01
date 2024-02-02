8866c085a52e132610362ab03d6b1863ff47971b
Evelyn Cristina <147438447+EvelynAires@users.noreply.github.com>
2024-02-02 23:41:59 +0000

    .

--- unidade 2/Listas/02-Arquivo/01.Questão/01-Questão.c
+++ unidade 2/Listas/02-Arquivo/01.Questão/01-Questão.c
@@ -1,3 +1,43 @@
 //1. Faca um programa em C que solicita ao usuario informacoes de funcionarios via teclado.
 // As informacoes digitadas pelo o usuario sao: id, nome e salaario do funcionario. Armazene as informacoes
 //digitadas pelo usuario em um arquivo texto.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct funcionarios{
+    int id;
+    char nome[25];
+    float salario;
+}Func;
+
+void preencher(){
+    FILE * entrada = fopen("Func.txt", "w");
+
+    if(entrada == NULL){
+        printf("Erro ao abrir!\n");
+        exit(1);
+    }
+
+
+Func funcionarios;
+printf("==Dados do Funcionario==\n");
+printf("Informe o id:");
+scanf("%d", &funcionarios.id);
+printf("Digite o nome: ");
+scanf(" %[^\n]", funcionarios.nome);
+printf("Informe o salario: ");
+scanf("%f", &funcionarios.salario);
+
+fprintf(entrada, "==DADOS DO FUNCIONARIO==\n");
+fprintf(entrada, "Id: %d\n", funcionarios.id);
+fprintf(entrada, "Nome: %s\n", funcionarios.nome);
+fprintf(entrada, "Salario: %.2f\n", funcionarios.salario);
+
+fclose(entrada);
+}
+
+int main(){
+    preencher();
+    return 0;
+}
