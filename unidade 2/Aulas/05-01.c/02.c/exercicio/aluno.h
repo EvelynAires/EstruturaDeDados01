b488e3a2f9d883cd36a5eb9bfe11940265d5ca63
Evelyn Cristina <147438447+EvelynAires@users.noreply.github.com>
2024-02-05 18:06:18 +0000

    .

---
+++ unidade 2/Aulas/05-01.c/02.c/exercicio/aluno.h
@@ -0,0 +1,5 @@
+//Definiçã de novo tipo: Aluno
+typedef struct  aluno Aluno;
+
+//Função que aloca memoria para um struct aluno, recebe os dados via teclado e retorna um ponteiro para Aluno
+Aluno * recebe_dados(void);
