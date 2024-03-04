c6b3b551c90490fd245c6f5bdaf23958312374b8
Evelyn Cristina <147438447+EvelynAires@users.noreply.github.com>
2024-03-04 17:52:56 +0000

    .

--- unidade 3/Aulas/ListaEncadeada/Lista.h
+++ unidade 3/Aulas/ListaEncadeada/Lista.h
@@ -1,8 +1,10 @@
 
 
 typedef struct lista Lista;
+
 /*
 Função que inicializa a lista
 atribuindo NULL
+Retorna: lista inicializada
 */
 Lista * cria_lista(void);
--- unidade 3/Aulas/ListaEncadeada/main.c
+++ unidade 3/Aulas/ListaEncadeada/main.c
@@ -1,7 +1,7 @@
 #include "lista.h"
 
 int main(void){
-
+ //Criando uma lista encadeada
     Lista * lista_encadeada = cria_lista();
     return 0;
 }
