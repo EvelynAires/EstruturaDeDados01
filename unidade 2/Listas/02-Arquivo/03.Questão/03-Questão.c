5d9fdf5afb90a02526d6664bd6fe46ca6747988b
Evelyn Cristina <147438447+EvelynAires@users.noreply.github.com>
2024-03-10 22:48:27 -0300

    Update 03-Questão.c

--- unidade 2/Listas/02-Arquivo/03.Questão/03-Questão.c
+++ unidade 2/Listas/02-Arquivo/03.Questão/03-Questão.c
@@ -14,3 +14,43 @@
 //Cada linha do arquivo deve conter o nome da fruta e seu preco, separados por vırgula.
 //O programa deve continuar solicitando os dados das frutas ate que o usuario decida parar.
 //Quando o usuario decidir parar de cadastrar frutas, o programa deve exibir uma mensagem de encerramento e fechar o arquivo.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct fruta {
+    char nome[30];
+    float preco;
+}Fruta;
+
+int main() {
+    FILE *arquivo;
+    Fruta fruta;
+    char continuar;
+    
+    arquivo = fopen("frutas.txt", "w");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+    
+    do {
+        printf("Informe o nome da fruta: ");
+        scanf(" %[^\n]s", fruta.nome);
+        printf("Digite o preco: ");
+        scanf("%f", &fruta.preco);
+        
+        // Escreve os dados da fruta no arquivo
+        fprintf(arquivo, "%s, %.2f\n", fruta.nome, fruta.preco);
+        
+        // Pergunta ao usuário se deseja cadastrar mais frutas
+        printf("Deseja cadastrar mais alguma fruta? (S/N): ");
+        scanf(" %c", &continuar);
+    } while (continuar == 'S' || continuar == 's');
+    
+    fclose(arquivo);
+    
+    printf("Cadastro de frutas finalizado. Verifique o arquivo 'frutas.txt'.\n");
+    
+    return 0;
+}
