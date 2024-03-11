d782d99e2b814058d17e59dd437c915b45e70f04
Evelyn Cristina <147438447+EvelynAires@users.noreply.github.com>
2024-03-10 22:57:21 -0300

    Update and rename questão.c to aluno.c

---
+++ unidade 2/Listas/03-TAD/02-QUESTÃO.c/aluno.c
@@ -0,0 +1,43 @@
+//Questão 2) Você foi contratado por uma instituição educacional para desenvolver um sistema
+//de gerenciamento de alunos e disciplinas. Para isso, você precisa criar um conjunto de Tipos
+//Abstratos de Dados (TADs) que permitam a manipulação eficiente dos dados dos alunos e das
+//disciplinas oferecidas pela instituição.
+#include "aluno.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+Aluno* cria_aluno(char nome[], int matricula) {
+    Aluno* aluno = (Aluno*)malloc(sizeof(Aluno));
+    if (aluno == NULL) {
+        printf("Erro ao alocar memória para o aluno.\n");
+        exit(1);
+    }
+    strcpy(aluno->nome, nome);
+    aluno->matricula = matricula;
+    aluno->num_disciplinas = 0;
+    return aluno;
+}
+
+void matricula_disciplina(Aluno* aluno, Disciplina* disciplina) {
+    for (int i = 0; i < aluno->num_disciplinas; i++) {
+        if (aluno->disciplinas[i]->codigo == disciplina->codigo) {
+            printf("O aluno já está matriculado nesta disciplina.\n");
+            return;
+        }
+    }
+
+   
+    if (aluno->num_disciplinas >= 10) {
+        printf("O aluno atingiu o limite de disciplinas.\n");
+        return;
+    }
+
+  
+    aluno->disciplinas[aluno->num_disciplinas] = disciplina;
+    aluno->num_disciplinas++;
+}
+
+void exclui_aluno(Aluno* aluno) {
+    free(aluno);
+}
--- unidade 2/Listas/03-TAD/02-QUESTÃO.c/questão.c
+++
@@ -1,4 +0,0 @@
-//Questão 2) Você foi contratado por uma instituição educacional para desenvolver um sistema
-//de gerenciamento de alunos e disciplinas. Para isso, você precisa criar um conjunto de Tipos
-//Abstratos de Dados (TADs) que permitam a manipulação eficiente dos dados dos alunos e das
-//disciplinas oferecidas pela instituição.
