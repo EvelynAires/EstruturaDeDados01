7198012e31ea430e3cb82d1c2abe461e6f7fd92e
Evelyn Cristina <147438447+EvelynAires@users.noreply.github.com>
2024-03-12 14:30:10 -0300

    .

---
+++ unidade 2/Aulas/31-01.c/calculadora.h
@@ -0,0 +1,10 @@
+
+float adicao(float primeiro_numero, float segundo_numero);
+
+float subtracao(float primeiro_numero, float segundo_numero);
+
+float multiplicacao(float primeiro_numero, float segundo_numero);
+
+float divisao(float primeiro_numero, float segundo_numero);
+
+float potenciacao(float base, float expoente);
