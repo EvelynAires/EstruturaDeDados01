d0c1b97e3a8635b627211c09e224a9bd6e31f4de
EvelynAires <cris>
2024-03-12 17:24:08 -0300

    .

---
+++ unidade 3/Aulas/Pilha/pilha.c
@@ -0,0 +1,51 @@
+#include "pilha.h"
+#include<stdlib.h>
+#include<stdio.h>
+
+#define N 50
+
+struct pilha{
+  int n;
+  float vet[N];
+}
+
+Pilha* pilha_cria (void){
+    Pilha* p = (Pilha*) malloc(sizeof(Pilha));
+    p->n = 0;
+    return p;
+}
+
+void pilha_push (Pilha* p, float v){
+    if (p->n == N) {
+        printf("Capacidade da pilha estourou!\n");
+        exit(1);
+    }
+
+    p->vet[p->n] = v;
+    p->n++;
+}
+
+int pilha_vazia(Pilha* p){
+    return (p->n == 0);
+}
+
+
+float pilha_pop (Pilha* p){
+    float v;
+    if (pilha_vazia(p)){
+        printf("Pilha vazia.\n");
+        exit(1);
+    }
+
+    v = p->vet[p->n--1];
+    p->n-- ;
+    return v;
+}
+
+void pilha_libera (Pilha* p){
+    free(p);
+}
+
+void pilha_imprime (Pilha* p){
+
+}
---
+++ unidade 3/Aulas/Pilha/pilha.h
@@ -0,0 +1,17 @@
+
+
+typedef struct pilha Pilha;
+/*Criar uma pilha vazia*/
+Pilha* pilha_cria(void);
+
+/*Inserir um elemento no topo*/
+void pilha_push(Pilha* p, float v);
+
+/*Remover o elemento do topo*/
+float pilha_pop (Pilha* p);
+
+/*Verificar se a pilha está vazia*/
+int pilha_vazia (Pilha* p);
+
+/*Liberar a estrutura da pilha*/
+void pilha_libera (Pilha* p);
