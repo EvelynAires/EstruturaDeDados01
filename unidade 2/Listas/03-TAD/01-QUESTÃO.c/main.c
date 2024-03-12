89ea8e5c7188153d14b14a14f58509ef15761962
Evelyn Cristina <147438447+EvelynAires@users.noreply.github.com>
2024-03-12 16:31:16 +0000

    .

--- unidade 2/Listas/03-TAD/01-QUESTÃO.c/main.c
+++ unidade 2/Listas/03-TAD/01-QUESTÃO.c/main.c
@@ -3,15 +3,15 @@
 
 int main() {
 
-    struct ContaBancaria *conta1 = cria_conta("Joao", 1234, 1000.0);
-    struct ContaBancaria *conta2 = cria_conta("Maria", 5678, 500.0);
+    struct ContaBancaria *conta1 = cria_conta("Hugo", 1234, 1000.0);
+    struct ContaBancaria *conta2 = cria_conta("Evelyn", 5678, 500.0);
 
     deposita(conta1, 200.0);
     saca(conta2, 100.0);
     transfere(conta1, conta2, 300.0);
 
-    printf("Saldo da conta de %s: %.2f\n", conta1->titular, saldo(conta1));
-    printf("Saldo da conta de %s: %.2f\n", conta2->titular, saldo(conta2));
+    printf("Saldo da conta total de %s: %.2f\n", conta1->titular, saldo(conta1));
+    printf("Saldo da conta total de %s: %.2f\n", conta2->titular, saldo(conta2));
 
     exclui_conta(conta1);
     exclui_conta(conta2);
